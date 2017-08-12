435893a4b75591d98f795e426b704115d44eb627
trupovar13 <trupovar13@gmail.com>
2017-08-12 20:28:25 +0300

    Add simple /details/... post handler

--- app.rb
+++ app.rb
@@ -77,3 +77,18 @@ get '/details/:post_id' do
 	erb :details
 end
 
+# обработчик post-запроса /details/...
+# (браузер отправляет данные на сервер, а мы их принимаем)
+post '/details/:post_id' do
+
+	#получаем переменную из URL`а
+	post_id = params[:post_id]
+
+	# получаем переменную из post запроса
+	content = params[:content]
+
+	erb "You typed comment #{content} for post #{post_id}"
+end
+
+
+
